564199b0cf76c139d03a5ed4bd842a09406ac57c
L Caraco <lbc.dna@gmail.com>
2021-05-02 16:30:15 -0500

    wrote eval?

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr {
@@ -12,17 +12,35 @@ pub enum Value {
     Unit,
     Int(i64),
     Float(f64),
-    Fn(Vec<String>, Box<Expr>),
+    Fn(VecDeque<String>, Box<Expr>),
 }
+
+// #[derive(Debug)]
+// pub struct Context {
+//     data: Vec<HashMap<String, Value>>,
+// }
+
+// impl Context {
+//     pub fn get(&self, k: &String) -> Result<Value, String> {
+//         for m in self.data.iter().rev() {
+//             if let Some(v) = m.get(k) {
+//                 return Ok(v.clone())
+//             }
+//         }
+//         Err(format!("NameError: \"{}\" not found", k))
+//     }
+
+//     // pub fn push(&mut self)
+// }
 
 #[derive(Debug)]
-pub struct Context {
-    data: HashMap<String, Expr>,
-    prev: Option<Box<Context>>,
+pub struct Context<'a> {
+    data: HashMap<String, Value>,
+    prev: Option<Box<&'a Context<'a>>>,
 }
 
-impl Context {
-    pub fn get(&self, k: &String) -> Result<Expr, String> {
+impl<'a> Context<'a> {
+    pub fn get(&self, k: &String) -> Result<Value, String> {
         if let Some(e) = self.data.get(k) {
             Ok(e.clone())
         } else if let Some(ctxt) = &self.prev {
@@ -31,14 +49,33 @@ impl Context {
             Err(format!("NameError: {}", k))
         }
     }
+
+    pub fn chain(&'a self, data: HashMap<String, Value>) -> Self {
+        Self { data, prev: Some(Box::new(self)) }
+    }
 }
 
 impl Expr {
     pub fn eval(&self, ctxt: &Context) -> Result<Value, String> {
         match self {
             Expr::Lit(v) => Ok(v.clone()),
-            Expr::Ident(id) => ctxt.get(id)?.eval(ctxt),
-            Expr::Form(h, tail) => todo!(),
+            Expr::Ident(id) => ctxt.get(id),
+            Expr::Form(h, tail) => {
+                match ctxt.get(h)? {
+                    Value::Fn(params, body) => {
+                        if tail.len() != params.len() {
+                            return Err(format!("Not enough args provided to {}: expected {}, got {}", h, params.len(), tail.len()))
+                        }
+                        let mut args = Vec::new();
+                        for a in tail {
+                            args.push(a.eval(ctxt)?);
+                        }
+                        let next = ctxt.chain(params.into_iter().zip(args).collect());
+                        body.as_ref().eval(&next)
+                    },
+                    v => Ok(v),
+                }
+            },
         }
     }
 }
