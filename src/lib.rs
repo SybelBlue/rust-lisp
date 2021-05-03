3d123206220b29a2de51fa983598e00b3e1e9b69
L Caraco <lbc.dna@gmail.com>
2021-05-03 12:42:39 -0500

    renamed to basics

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,14 @@ pub mod context;
 #[cfg(test)]
 mod tests {
     mod lexer {
-        // use std::collections::VecDeque;
+        use crate::parser::exec;
 
-        // use crate::{lexer::*, parser::{Expr, Value}};
+        #[test]
+        fn basics() {
+            let s = std::fs::read_to_string("test/basics.rsp").expect("file not found");
+            let (reses, ctxt) = exec(s);
+            println!("{:?}", reses);
+            assert!(false)
+        }
     }
 }
