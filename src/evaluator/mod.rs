9ad4cfec2a8c7a22569e492bd5a642ff2b8a2609
L Caraco <lbc.dna@gmail.com>
2021-05-04 15:53:00 -0500

    migrated BIFn to value

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -50,28 +50,6 @@ pub struct BuiltInFn {
     pub f: fn(&Context<'_>, Vec<Token>) -> EvalResult<Value>,
 }
 
-impl BuiltInFn {
-    pub fn new(name: &str, f: fn(&Context<'_>, Vec<Token>) -> EvalResult<Value>) -> Value {
-        Value::BuiltIn(Self::simple(String::from(name), f))
-    }
-
-    pub fn simple(name: String, f: fn(&Context<'_>, Vec<Token>) -> EvalResult<Value>) -> Self {
-        BuiltInFn { name, f }
-    }
-}
-
-impl std::fmt::Debug for BuiltInFn {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "BIBody({})", self.name)
-    }
-}
-
-impl std::cmp::PartialEq for BuiltInFn {
-    fn eq(&self, other: &Self) -> bool {
-        self.name == other.name
-    }
-}
-
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr {
     Lit(Value),
--- src/evaluator/value.rs
+++ src/evaluator/value.rs
@@ -98,3 +98,25 @@ impl PartialEq for Value {
         }
     }
 }
+
+impl BuiltInFn {
+    pub fn new(name: &str, f: fn(&Context<'_>, Vec<Token>) -> EvalResult<Value>) -> Value {
+        Value::BuiltIn(Self::simple(String::from(name), f))
+    }
+
+    pub fn simple(name: String, f: fn(&Context<'_>, Vec<Token>) -> EvalResult<Value>) -> Self {
+        BuiltInFn { name, f }
+    }
+}
+
+impl std::fmt::Debug for BuiltInFn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "BIBody({})", self.name)
+    }
+}
+
+impl std::cmp::PartialEq for BuiltInFn {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
