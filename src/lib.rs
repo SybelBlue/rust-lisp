da63e103de8015d8faa2df806a98cc942a9f674d
L Caraco <lbc.dna@gmail.com>
2021-05-04 09:37:20 -0500

    deindent

--- src/lib.rs
+++ src/lib.rs
@@ -4,86 +4,84 @@ pub mod context;
 
 #[cfg(test)]
 mod tests {
-    mod lexer {
-        use crate::evaluator::{*, Expr::*, Value::*};
+    use crate::evaluator::{*, Expr::*, Value::*};
 
-        #[test]
-        fn basics() {
-            let s = std::fs::read_to_string("test/basics.rsp").expect("file not found");
-            let (reses, _) = exec(s);
-            for (ex, ac) in vec![ Unit
-                , Int(3)
-                , Unit
-                , Int(4)
-                , Unit
-                , Int(2)
-                , Float(6.1)
-                , Unit
-                , Int(-1)
-                , Unit
-                , Int(2)
-                , Fn(
-                    vec![ Ident { name: format!("a"), file_pos: FilePos { col: 6, line: 23 } }
-                        , Ident { name: format!("b"), file_pos: FilePos { col: 8, line: 23 } }
-                        ]
-                    , None
-                    , Box::new(Token { 
-                        expr: Form(
-                            vec![ Token { expr: Var(format!("+")), file_pos: FilePos { col: 12, line: 23 } }
-                                , Token { expr: Var(format!("a")), file_pos: FilePos { col: 14, line: 23 } }
-                                , Token { expr: Var(format!("b")), file_pos: FilePos { col: 16, line: 23 } }
-                                ]), 
-                        file_pos: FilePos { col: 11, line: 19 } }))
+    #[test]
+    fn basics() {
+        let s = std::fs::read_to_string("test/basics.rsp").expect("file not found");
+        let (reses, _) = exec(s);
+        for (ex, ac) in vec![ Unit
+            , Int(3)
+            , Unit
+            , Int(4)
+            , Unit
+            , Int(2)
+            , Float(6.1)
+            , Unit
+            , Int(-1)
+            , Unit
+            , Int(2)
+            , Fn(
+                vec![ Ident { name: format!("a"), file_pos: FilePos { col: 6, line: 23 } }
+                    , Ident { name: format!("b"), file_pos: FilePos { col: 8, line: 23 } }
+                    ]
+                , None
+                , Box::new(Token { 
+                    expr: Form(
+                        vec![ Token { expr: Var(format!("+")), file_pos: FilePos { col: 12, line: 23 } }
+                            , Token { expr: Var(format!("a")), file_pos: FilePos { col: 14, line: 23 } }
+                            , Token { expr: Var(format!("b")), file_pos: FilePos { col: 16, line: 23 } }
+                            ]), 
+                    file_pos: FilePos { col: 11, line: 19 } }))
+        ].into_iter().zip(reses.into_iter()) {
+            assert_eq!(Ok(ex), ac)
+        }
+    }
+
+    #[test]
+    fn basic_errs() {
+        use crate::{parser::ParseError::*, evaluator::Error::*};
+        let s = std::fs::read_to_string("test/basic-errs.rsp").expect("file not found");
+        let (reses, _) = exec(s);
+        let mut i = 0;
+        assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
+        assert!(matches!(reses[i], Ok(Unit))); i += 1;
+        assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
+        assert!(matches!(reses[i], Ok(Unit))); i += 1;
+        assert!(matches!(reses[i], Err(RedefError(_, _)))); i += 1;
+        assert!(matches!(reses[i], Err(ArgError { .. } ))); i += 1;
+        assert!(matches!(reses[i], Err(ValueError(_, _)))); i += 1;
+        assert!(matches!(reses[i], Err(ValueError(_, _)))); i += 1;
+        assert!(matches!(reses[i], Err(ArgError { .. } ))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(BadChar(_,_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(DupArg { .. })))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(BadQuote(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(BadQuote(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(BadQuote(_,_))))); i += 1;
+        assert!(matches!(reses[i], Err(IllegalDefError(_)))); i += 1;
+        assert!(matches!(reses[i], Err(RedefError(_, _)))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(DupArg { .. })))); i += 1;
+        assert!(matches!(reses[i], Err(ParseError(Eof(_))))); i += 1;
+        assert_eq!(None, reses.get(i))
+    }
+
+    #[test]
+    fn complex_fns() {
+        let s = std::fs::read_to_string("test/funcs.rsp").expect("file not found");
+        let (reses, _) = exec(s);
+        for (ex, ac) in vec![ Unit
+            , Unit
+            , Int(3)
+            , Unit
+            , Int(1)
             ].into_iter().zip(reses.into_iter()) {
-                assert_eq!(Ok(ex), ac)
-            }
-        }
-
-        #[test]
-        fn basic_errs() {
-            use crate::{parser::ParseError::*, evaluator::Error::*};
-            let s = std::fs::read_to_string("test/basic-errs.rsp").expect("file not found");
-            let (reses, _) = exec(s);
-            let mut i = 0;
-            assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
-            assert!(matches!(reses[i], Ok(Unit))); i += 1;
-            assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
-            assert!(matches!(reses[i], Ok(Unit))); i += 1;
-            assert!(matches!(reses[i], Err(RedefError(_, _)))); i += 1;
-            assert!(matches!(reses[i], Err(ArgError { .. } ))); i += 1;
-            assert!(matches!(reses[i], Err(ValueError(_, _)))); i += 1;
-            assert!(matches!(reses[i], Err(ValueError(_, _)))); i += 1;
-            assert!(matches!(reses[i], Err(ArgError { .. } ))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(NameError(_)))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(BadChar(_,_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(Missing(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(DupArg { .. })))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(BadQuote(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(BadQuote(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(BadQuote(_,_))))); i += 1;
-            assert!(matches!(reses[i], Err(IllegalDefError(_)))); i += 1;
-            assert!(matches!(reses[i], Err(RedefError(_, _)))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(DupArg { .. })))); i += 1;
-            assert!(matches!(reses[i], Err(ParseError(Eof(_))))); i += 1;
-            assert_eq!(None, reses.get(i))
-        }
-
-        #[test]
-        fn complex_fns() {
-            let s = std::fs::read_to_string("test/funcs.rsp").expect("file not found");
-            let (reses, _) = exec(s);
-            for (ex, ac) in vec![ Unit
-                , Unit
-                , Int(3)
-                , Unit
-                , Int(1)
-                ].into_iter().zip(reses.into_iter()) {
-                assert_eq!(Ok(ex), ac)
-            }
+            assert_eq!(Ok(ex), ac)
         }
     }
 }
