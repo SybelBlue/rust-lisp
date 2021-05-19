4891503e4c16a361777a080db23fbe11a8f36e64
L Caraco <lbc.dna@gmail.com>
2021-05-19 13:22:01 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,33 @@ use rust_lisp::{context::Context, evaluator::{exec, exec_using}};
 //      list-likes (iterator, sll)
 
 fn main() -> std::io::Result<()> {
-    let path = std::env::args().nth(1).expect("Need path to file");
-    println!("Loading {}...", path);
-    let mut ctxt = if let Ok(s) = std::fs::read_to_string(path.as_str()) {
-        let (reses, ctxt) = exec(s.clone());
-        for r in reses {
-            match r {
-                Ok(v) => println!(" | {}", v),
-                Err(e) => println!(" * {} ", e),
+    let arg = std::env::args().nth(1);
+    let mut ctxt = arg.map_or_else(Context::new, |path| {
+        match std::fs::read_to_string(path.as_str()) {
+            Ok(s) => {
+                println!("Loading {}...", path);
+
+                let (reses, ctxt) = exec(s.clone());
+                for r in reses {
+                    match r {
+                        Ok(v) => println!(" | {}", v),
+                        Err(e) => println!(" * {} ", e),
+                    }
+                }
+
+                println!("Loaded {} w/ {} symbols", path, ctxt.size());
+
+                ctxt
+            },
+            Err(e) => {
+                println!("Loading {} failed with\n{}", path, e);
+                
+                Context::new()
             }
         }
+    });
 
-        println!("Loaded {} w/ {} symbols", path, ctxt.size());
-
-        ctxt
-    } else {
-        Context::new()
-    };
-
-    let reader = Interface::new("risp")?;
+    let reader = Interface::new("risp-repl")?;
     
     reader.set_prompt(">> ")?;
     
@@ -38,7 +46,7 @@ fn main() -> std::io::Result<()> {
         for r in reses {
             match r {
                 Ok(v) => println!(" | {}", v),
-                Err(e) => println!(" * {} ", e),
+                Err(e) => println!("** {} ", e),
             }
         }
     }
