50e22f25b80c99f9c7fc31824ddd08eff64f63cc
Logan Caraco <lbc.dna@gmail.com>
2022-02-16 18:17:08 -0800

    removed everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,6 @@
-pub mod context;
-pub mod expr;
-pub mod lexer;
-pub mod parser;
-pub mod result;
-pub mod rtype;
-pub mod token;
-pub mod value;
-
-pub(crate) mod builtin_fn;
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn success() {
-        // z3 = {version="0.11.2", features = ["static-link-z3"]}
-        // use z3::*;
     }
 }
