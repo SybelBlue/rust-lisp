ee465ede9a5482ea4fb187b93d2aa129b87b8f61
Logan Caraco <lbc.dna@gmail.com>
2022-02-19 19:32:29 -0800

    cleaned up lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod parsing;
+pub mod parsing;
 
 #[cfg(test)]
 mod tests {
@@ -6,7 +6,7 @@ mod tests {
     fn basic() {
         use crate::parsing::lex::{Token::{self, *}, Source};
 
-        let src = format!("()\nhello\n(+ 12 34 53) (    test\n\t\n\n 2 )");
+        let src = format!("()\nhello\n(+ 12 34 53) (    test\n\t\n\n 2 ) (0 (1 (2 (3)) ((4) 5)) 6)");
         let mut source = Source::new(&src, None);
 
         fn word(s: &str) -> Token { Word(String::from(s)) }
@@ -18,6 +18,13 @@ mod tests {
                 , word("hello")
                 , SExp(vec![word("+"), word("12"), word("34"), word("53")])
                 , SExp(vec![word("test"), word("2")])
-                ]))
+                , SExp(vec![word("0")
+                    , SExp(vec![word("1")
+                        , SExp(vec![word("2")
+                            , SExp(vec![word("3")])])
+                        , SExp(vec![SExp(vec![word("4")])
+                            , word("5")])
+                        ])
+                    , word("6")])]))
     }
 }
--- src/parsing/lex.rs
+++ src/parsing/lex.rs
@@ -1,8 +1,6 @@
-use std::{iter::Peekable, str::Chars};
-
 #[derive(Debug)]
 pub struct Source<'a> {
-    pub(crate) txt: Peekable<Chars<'a>>,
+    txt: std::str::Chars<'a>,
     pub name: Option<&'a String>
 }
 
@@ -22,58 +20,113 @@ pub type LexResult<T> = Result<T, LexError>;
 
 impl<'a> Source<'a> {
     pub fn new(source: &'a String, name: Option<&'a String>) -> Self {
+        Self { txt: source.chars(), name }
+    }
+
+    pub fn lex(&mut self) -> LexResult<Vec<Token>> {        
+        let mut stack = LexStack::new();
+
+        loop {
+            let (skipped, next) = self.advance();
+            
+            if skipped {
+                stack.try_push_word();
+            }
+            
+            match next {
+                None => break,
+                Some('(') => stack.open_sexp(),
+                Some(')') => {
+                    let sexp = stack.close_sexp()?;
+                    stack.push_token(Token::SExp(sexp));
+                },
+                Some(ch) => stack.push_char(ch),
+            }
+        }
+
+        stack.try_push_word();
+
+        stack.finish()
+    }
+
+    fn advance(&mut self) -> (bool, Option<char>) {
+        let mut skipped = false;
+        loop {
+            let n = self.txt.next();
+            if !matches!(n, Some(c) if c.is_whitespace()) {
+                return (skipped, n);
+            } else {
+                skipped = true;
+            }
+        }
+    }
+}
+
+/// Used to store 
+#[derive(Debug)]
+struct LexStack {
+    sexp_stack: Vec<Vec<Token>>,
+    finished: Vec<Token>,
+    curr_word: String,
+}
+
+impl LexStack {
+    fn new() -> Self {
         Self {
-            txt: source.chars().peekable(),
-            name,
+            sexp_stack: Vec::with_capacity(8),      // 8 deep nested sexp
+            finished: Vec::with_capacity(50),       // 50 tokens on top level
+            curr_word: String::with_capacity(10),   // 10 char words
         }
     }
 
-    pub fn lex(&mut self) -> LexResult<Vec<Token>> {
-        let mut out = Vec::with_capacity(50);
-        let mut sexp_stack: Vec<Vec<Token>> = Vec::with_capacity(6);
-        let mut curr = String::with_capacity(10);
+    fn open_sexp(&mut self) {
+        self.sexp_stack.push(Vec::with_capacity(4)) // 4 long sexp
+    }
 
-        fn dump_curr(curr: &mut String) -> Token {
-            let out = Token::Word(curr.clone());
-            curr.clear();
-            out
+    fn close_sexp(&mut self) -> LexResult<Vec<Token>> {
+        let last_sexp = self.sexp_stack.pop();
+        let mut finished = last_sexp.ok_or(LexError::TooManyClosing)?;
+
+        if self.mid_word() {
+            finished.push(self.dump_curr());
         }
 
-        loop {
-            match self.txt.next() {
-                None => break,
-                Some('(') => sexp_stack.push(Vec::with_capacity(4)),
-                Some(')') => {
-                    let mut finished = sexp_stack.pop().ok_or(LexError::TooManyClosing)?;
-                    
-                    if !curr.is_empty() {
-                        finished.push(dump_curr(&mut curr));
-                    }
-                    
-                    sexp_stack.last_mut().unwrap_or(&mut out).push(Token::SExp(finished));
-                },
-                Some(ch) if ch.is_whitespace() => 
-                    sexp_stack.last_mut().unwrap_or(&mut out).push(dump_curr(&mut curr)),
-                Some(ch) => {
-                    curr.push(ch);
-                    continue;
-                }
-            }
-            self.skip_whitespace();
-        }
+        Ok(finished)
+    }
 
-        if !curr.is_empty() {
-            out.push(Token::Word(curr));
-        }
+    fn push_token(&mut self, t: Token) {
+        self.sexp_stack
+            .last_mut()
+            .unwrap_or(&mut self.finished)
+            .push(t)
+    }
 
-        if sexp_stack.is_empty() {
-            Ok(out)
+    fn dump_curr(&mut self) -> Token {
+        let out = Token::Word(self.curr_word.clone());
+        self.curr_word.clear();
+        out
+    }
+
+    fn push_char(&mut self, ch: char) {
+        self.curr_word.push(ch)
+    }
+
+    fn try_push_word(&mut self) {
+        if self.mid_word() {
+            let t = self.dump_curr();
+            self.push_token(t);
+        }
+    }
+
+    fn mid_word(&self) -> bool {
+        !self.curr_word.is_empty()
+    }
+
+    fn finish(self) -> LexResult<Vec<Token>> {
+        if self.sexp_stack.is_empty() {
+            Ok(self.finished)
         } else {
             Err(LexError::Unclosed)
         }
-    }
-
-    fn skip_whitespace(&mut self) {
-        while self.txt.next_if(|c| c.is_whitespace()).is_some() {}
     }
 }
