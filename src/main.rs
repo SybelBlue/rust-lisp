cad7ee3588af514d247b60993fe1c484c88a56a9
Logan Caraco <lbc.dna@gmail.com>
2022-02-19 19:32:24 -0800

    added main

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,19 @@
-fn main() {
+use linefeed::{Interface, ReadResult};
+
+use rust_lisp::parsing::lex::Source;
+
+fn main() -> std::io::Result<()> {
+    let reader = Interface::new("risp-repl")?;
+    let ref name = format!("term-input");
+    reader.set_prompt(">> ")?;
+    
+    while let ReadResult::Input(input) = reader.read_line()? {
+        match Source::new(&input, Some(name)).lex() {
+            Ok(v) => println!("{:?}", v),
+            Err(e) => println!("** {:?}", e)
+        }
+    }
+    
+    // println!("Batch finished w/ {} symbols", ctxt.size());
+    Ok(())
 }
