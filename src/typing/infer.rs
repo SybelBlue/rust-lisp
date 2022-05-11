76c329882974d6965cf6f959e9d7f7a3519d3c6c
Logan Caraco <lbc.dna@gmail.com>
2022-05-10 20:43:08 -0700

    basic sexp typing

--- src/typing/infer.rs
+++ src/typing/infer.rs
@@ -44,8 +44,9 @@ impl Infer {
 
 lazy_static::lazy_static! {
     static ref NULL: Vec<Constraint> = Vec::with_capacity(0);
+    static ref UNIT_TYPE: Type = Type::Data(String::from("Unit"), Vec::with_capacity(0));
     static ref NAT_TYPE: Type = Type::Data(String::from("Nat"), Vec::with_capacity(0));
-    static ref CHR_TYPE: Type = Type::Data(String::from("Char"), Vec::with_capacity(0));
+    static ref CHAR_TYPE: Type = Type::Data(String::from("Char"), Vec::with_capacity(0));
 }
 
 #[allow(dead_code)]
@@ -56,7 +57,7 @@ pub(crate) fn infer_expr<'a>(infer: Infer, e: &'a Expr<'a>) -> InferResult<'a, (
             let VToken { body, pos } = v;
             match body {
                 Nat(_)  => Ok((infer, (NAT_TYPE.clone(), NULL.clone()))),
-                Char(_) => Ok((infer, (CHR_TYPE.clone(), NULL.clone()))),
+                Char(_) => Ok((infer, (CHAR_TYPE.clone(), NULL.clone()))),
                 Sym(k) => 
                     lookup_env(infer, k, pos)
                         .map(|(i, t)| (i, (t, NULL.clone()))),
@@ -75,7 +76,34 @@ pub(crate) fn infer_expr<'a>(infer: Infer, e: &'a Expr<'a>) -> InferResult<'a, (
                 }
             }
         }
-        Expr::SExp(_) => todo!(),
+        Expr::SExp(SToken { body: es, .. }) => {
+            let mut es = es.into_iter();
+            let f_expr = if let Some(fst) = es.next() {
+                fst
+            } else {
+                return Ok((infer, (UNIT_TYPE.clone(), NULL.clone())));
+            };
+            let (mut infer, (f_type, mut cs)) = infer_expr(infer, f_expr)?;
+            let mut arg_types = Vec::new();
+            for e in es {
+                let (new_infer, (b_type, b_cs)) = infer_expr(infer, e)?;
+                infer = new_infer;
+                cs.extend(b_cs.into_iter());
+                arg_types.push(b_type);
+            }
+            let ret_type = Type::Var(infer.fresh());
+            let full_f_type = 
+                arg_types.into_iter()
+                    .rev()
+                    .fold(
+                        ret_type.clone(), 
+                        |prev, arg| Type::fun(arg, prev)
+                    );
+            
+            cs.push((f_type, full_f_type));
+
+            Ok((infer, (ret_type, cs)))
+        },
     }
 }
 
