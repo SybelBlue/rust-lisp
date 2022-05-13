285d2b88082d637ff81d12a28f155bb850eeb8b0
Logan Caraco <lbc.dna@gmail.com>
2022-05-13 01:23:12 -0700

    revert to using infer_top

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use linefeed::{Interface, ReadResult};
 
-use rust_lisp::{parsing::{sources::Source, parse}, typing::infer::{Infer, infer_mod}, errors::{LexError, LexErrorBody}};
+use rust_lisp::{parsing::{sources::Source, parse}, typing::infer::{Infer, infer_top}, errors::{LexError, LexErrorBody}};
 
 fn main() -> std::io::Result<()> {
     let reader = Interface::new("risp-repl")?;
@@ -26,8 +26,8 @@ fn main() -> std::io::Result<()> {
             Ok(ts) => {
                 match parse(ts) {
                     Ok(stmts) => {
-                        match infer_mod(Infer::new(), &stmts) {
-                            Ok(ts) => {
+                        match infer_top(Infer::new(), &stmts) {
+                            Ok((_, ts)) => {
                                 // ctxt = new;
                                 // reader.set_completer(Arc::new(ctxt.clone()));
                                 for (e, t) in stmts.iter().zip(ts) {
